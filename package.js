379e7e4d507ad7da3cd2397197638302721c5e2c
Marcelo Reyna <Lepozepo@gmail.com>
2014-08-27 20:40:35 -0700

    Fix dependencies
    .versionsFrom doesn’t seem to work as intended

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
 	name:"lepozepo:s3",
 	summary: "Upload files to S3. Allows use of Knox Server-Side.",
-	version:"3.0.0",
+	version:"3.0.1",
 	git:"https://github.com/Lepozepo/S3"
 });
 
@@ -11,8 +11,9 @@ Npm.depends({
 
 Package.on_use(function (api) {
 	//Need service-configuration to use Meteor.method
-	api.use(["underscore", "ejson","service-configuration"], ["client", "server"]);
-	api.use(["ui","templating","spacebars"], "client");
+	// api.versionsFrom('METEOR-CORE@0.9.0-atm');
+	api.use(["underscore@1.0.0", "ejson@1.0.0","service-configuration@1.0.0"], ["client", "server"]);
+	api.use(["ui@1.0.0","templating@1.0.0","spacebars@1.0.0"], "client");
 	api.add_files("client/blocks.html", "client");
 	api.add_files("client/events.js", "client");
 	api.add_files("s3server.js", "server");
