431f3a5415b2dbde1215273595e34933cc7603f8
Marcelo Reyna <Lepozepo@gmail.com>
2015-02-25 20:44:25 -0800

    Upgrade dependencies and remove trash

--- package.js
+++ package.js
@@ -1,19 +1,21 @@
 Package.describe({
 	name:"lepozepo:s3",
 	summary: "Upload files to S3. Allows use of Knox Server-Side.",
-	version:"4.1.3",
+	version:"5.0.0",
 	git:"https://github.com/Lepozepo/S3"
 });
 
 Npm.depends({
-	knox: "0.9.1",
-	"stream-buffers":"0.2.5",
-	"aws-sdk":"2.0.19"
+	knox: "0.9.2",
+	"stream-buffers":"2.1.0",
+	"aws-sdk":"2.1.14"
 });
 
 Package.on_use(function (api) {
-	api.use(["underscore@1.0.0", "ejson@1.0.0","service-configuration@1.0.0","coffeescript@1.0.0","lepozepo:streams@0.2.0"], ["client", "server"]);
-	api.use(["ui@1.0.0","templating@1.0.0","spacebars@1.0.0"], "client");
+	api.versionsFrom('METEOR@1.0');
+
+	api.use(["underscore", "check","coffeescript","service-configuration","lepozepo:streams@0.2.0"], ["client", "server"]);
+	// api.use(["ui@1.0.0","templating@1.0.0","spacebars@1.0.0"], "client");
 
 	// Client
 	api.add_files("client/functions.coffee", "client");
