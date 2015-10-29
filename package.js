d15dcac2c913cc581c7cb765374c5d95db5b5d94
Marcelo Reyna <lepozepo@gmail.com>
2015-10-28 21:08:08 -0700

    Update package version

--- package.js
+++ package.js
@@ -14,7 +14,7 @@ Npm.depends({
 Package.on_use(function (api) {
 	api.versionsFrom('METEOR@1.0');
 
-	api.use(["meteor-base","coffeescript","service-configuration"], ["client", "server"]);
+	api.use(["meteor-base@1.0.1","coffeescript","service-configuration"], ["client", "server"]);
 	api.use(["check","random"], ["client","server"]);
 
 	// Client
