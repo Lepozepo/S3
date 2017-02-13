df001a6ebd6efbf4e470d9b1a3f589bc8c7cdb76
Marcelo Reyna <lepozepo@gmail.com>
2017-02-13 09:39:53 -0800

    Version bump

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
 	name:"lepozepo:s3",
 	summary: "Upload files to S3. Allows use of Knox Server-Side.",
-	version:"5.2.4",
+	version:"5.2.5",
 	git:"https://github.com/Lepozepo/S3"
 });
 
